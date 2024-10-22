d84ec298920876b3203841e359c12c45fbfab8de
AngelMFran <159744468+AngelMFran@users.noreply.github.com>
2024-10-21 18:57:45 -0600

    Update db.php

--- proyectodesarrollo/db.php
+++ proyectodesarrollo/db.php
@@ -1,16 +1,18 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// Configuración de la conexión a la base de datos para el entorno local
+$servername = "localhost";
+$username = "root";  // Usualmente en local es 'root', cámbialo si es diferente
+$password = "";  // Deja vacío si no tienes contraseña en tu entorno local
+$database = "crudbddv2_local";  // Nombre de tu base de datos local
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-// Crear la conexión
-$conn = new mysqli($server, $username, $password, $db);
+// Crear conexión a MySQL
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Verificar la conexión
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
+
+// Establecer el conjunto de caracteres a UTF-8
+$conn->set_charset("utf8");
 ?>
