e85986b86fc7df0033d0c1f63239faf3ccdec7ac
AngelMFran <159744468+AngelMFran@users.noreply.github.com>
2024-10-21 18:49:06 -0600

    Update db.php

--- proyectodesarrollo/db.php
+++ proyectodesarrollo/db.php
@@ -1,16 +1,31 @@
 <?php
-// Configuración de la conexión a la base de datos local
-$servername = "127.0.0.1";
+// ConfiguraciÃ³n de la conexiÃ³n a la base de datos local
+$servername = "127.0.0.1";<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+// Verificar la conexiÃ³n
+if ($conn->connect_error) {
+    die("ConexiÃ³n fallida: " . $conn->connect_error);
+}
+?>
+
 $username = "root";
-$password = ""; // Si tienes contraseña en tu servidor local, inclúyela aquí
+$password = ""; // Si tienes contraseÃ±a en tu servidor local, inclÃºyela aquÃ­
 $database = "crudbddv2_local";
 
-// Crear conexión
+// Crear conexiÃ³n
 $conn = new mysqli($servername, $username, $password, $database, 3306);
 
-// Verificar la conexión
+// Verificar la conexiÃ³n
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexiÃ³n: " . $conn->connect_error);
 }
 
 // Establecer el conjunto de caracteres a UTF-8
