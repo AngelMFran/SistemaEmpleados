d46c3f06771346f76cff345de9ef82f11cb522a0
AngelMFran <159744468+AngelMFran@users.noreply.github.com>
2024-10-21 20:37:24 -0600

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,11 +2,11 @@
 // app/config/config.php
 return [
     'database' => [
-        'adapter'  => 'Mysql',
-        'host'     => 'localhost',
-        'port'     => 3307, // Cambia este puerto si es necesario
-        'username' => 'root',
-        'password' => '',
-        'dbname'   => 'crudbddv2_local',
+        'adapter'  => 'Postgresql',
+        'host'     => 'dpg-csbguetds78s73b7bmh0-a',  // Host proporcionado
+        'port'     => 5432,                          // Puerto predeterminado para PostgreSQL
+        'username' => 'crudbddv2_local_user',        // Usuario proporcionado
+        'password' => 'swMa4AwhunT2VYLUXr5Dqux5xg2UvzMr', // Contraseña proporcionada
+        'dbname'   => 'crudbddv2_local',             // Nombre de la base de datos proporcionado
     ],
 ];
